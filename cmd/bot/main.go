decc749a7de9e050ad0c7557d5b53ea7b0e2d4e0
BoburF <changeitgo3@gmail.com>
2025-09-07 12:47:01 +0500

    new package and echo bot

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,19 +1,19 @@
 package main
 
 import (
+	"context"
 	"log"
-	"net/http"
 	"os"
-	"os/exec"
+	"os/signal"
 
-	botcommands "github.com/BoburF/golang-bot-converter/cmd/bot/bot-commands"
-	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/go-telegram/bot"
+	"github.com/go-telegram/bot/models"
 	"github.com/joho/godotenv"
 )
 
 func main() {
 	if err := godotenv.Load(); err != nil {
-		log.Println("No .env file found, relying on system environment variables")
+		log.Fatal("No .env file found, relying on system environment variables")
 	}
 
 	token := os.Getenv("TELEGRAM_TOKEN")
@@ -21,73 +21,25 @@ func main() {
 		log.Fatal("TELEGRAM_TOKEN is not set")
 	}
 
-	bot, err := tgbotapi.NewBotAPI(token)
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	opts := []bot.Option{
+		bot.WithDefaultHandler(handler),
+	}
+
+	b, err := bot.New(token, opts...)
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 
-	log.Printf("Authorized on account %s", bot.Self.UserName)
-
-	u := tgbotapi.NewUpdate(0)
-	u.Timeout = 60
-
-	botcommands.SetCommands(bot)
-
-	updates := bot.GetUpdatesChan(u)
-
-	for update := range updates {
-		if update.Message == nil {
-			continue
-		}
-
-		if len(update.Message.Photo) > 0 {
-			photo := update.Message.Photo[len(update.Message.Photo)-1]
-			file, err := bot.GetFile(tgbotapi.FileConfig{FileID: photo.FileID})
-			if err != nil {
-				log.Println("Error getting file:", err)
-				continue
-			}
-
-			url := file.Link(token)
-			inputFile := "input.png"
-			outputFile := "output.jpg"
-
-			if err := downloadFile(inputFile, url); err != nil {
-				log.Println("Error downloading file:", err)
-				continue
-			}
-
-			cmd := exec.Command("ffmpeg", "-y", "-i", inputFile, outputFile)
-			if err := cmd.Run(); err != nil {
-				log.Println("ffmpeg error:", err)
-				continue
-			}
-
-			photoMsg := tgbotapi.NewPhoto(update.Message.Chat.ID, tgbotapi.FilePath(outputFile))
-			bot.Send(photoMsg)
-
-			os.Remove(inputFile)
-			os.Remove(outputFile)
-		} else {
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Привет, "+update.Message.From.FirstName+"! Пришли фото, и я сконвертирую его в JPG.")
-			bot.Send(msg)
-		}
-	}
+	log.Println("Bot has started...")
+	b.Start(ctx)
 }
 
-func downloadFile(filepath string, url string) error {
-	out, err := os.Create(filepath)
-	if err != nil {
-		return err
-	}
-	defer out.Close()
-
-	resp, err := http.Get(url)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-
-	_, err = out.ReadFrom(resp.Body)
-	return err
+func handler(ctx context.Context, b *bot.Bot, update *models.Update) {
+	b.SendMessage(ctx, &bot.SendMessageParams{
+		ChatID: update.Message.Chat.ID,
+		Text:   update.Message.Text,
+	})
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/BoburF/golang-bot-converter
 go 1.24.5
 
 require (
-	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
+	github.com/go-telegram/bot v1.17.0
 	github.com/joho/godotenv v1.5.1
 )
